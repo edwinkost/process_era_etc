a35c3b7ceff9760da2b853dfcc4db5b8588d4ff5
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-04-23 00:25:01 +0200

    start

--- process_era5-land/download_global_2017.sh
+++ process_era5-land/download_global_2017.sh
@@ -1,21 +1,15 @@
 
 set -x
 
-# -year 2000, months 1 to 12
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ temp 2000 2000 1 12 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ wind 2000 2000 1 12 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ sp   2000 2000 1 12 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ tp   2000 2000 1 12 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ ssr  2000 2000 1 12 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ fa   2000 2000 1 12 0 0 28 -16 41 &
+#~ # -year 2017, months 1 to 12
+#~ python download_era5-land_global.py /scratch-shared/edwinhs/era5-land_meteo/hourly/ temp 2017 2017 1 12 0 &
+#~ python download_era5-land_global.py /scratch-shared/edwinhs/era5-land_meteo/hourly/ wind 2017 2017 1 12 0 &
+#~ python download_era5-land_global.py /scratch-shared/edwinhs/era5-land_meteo/hourly/ tp   2017 2017 1 12 0 &
 
-# -year 2001, month 1 only
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ temp 2001 2001 1  1 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ wind 2001 2001 1  1 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ sp   2001 2001 1  1 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ tp   2001 2001 1  1 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ ssr  2001 2001 1  1 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ fa   2001 2001 1  1 0 0 28 -16 41 &
+# -year 2018, month 1 only
+python download_era5-land_global.py /scratch-shared/edwinhs/era5-land_meteo/hourly/ temp 2018 2018 1  1 0 &
+python download_era5-land_global.py /scratch-shared/edwinhs/era5-land_meteo/hourly/ wind 2018 2018 1  1 0 &
+python download_era5-land_global.py /scratch-shared/edwinhs/era5-land_meteo/hourly/ tp   2018 2018 1  1 0 &
 
 wait
 
