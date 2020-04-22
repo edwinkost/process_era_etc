d0b71f287f60bd0493ce055b430b540fbefe30bf
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-04-23 01:31:27 +0200

    check

--- process_era5-land/download_global_2016-2018.sh
+++ process_era5-land/download_global_2016-2018.sh
@@ -19,7 +19,7 @@ set -x
 # -year 2019, month 1 only
 python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp 2019 2019 1  1 0 &
 python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind 2019 2019 1  1 0 &
-python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp   2019 2019 1  1 0 &
+#~ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp   2019 2019 1  1 0 &
 
 wait
 
