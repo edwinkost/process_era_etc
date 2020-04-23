ebff03c012abcb6a179f66b1f144cddb70cea897
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-04-23 10:56:18 +0200

    start downloading - 2016-2020

--- process_era5-land/download_global_2016-2020.sh
+++ process_era5-land/download_global_2016-2020.sh
@@ -19,11 +19,17 @@ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hour
 python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_v 2018 2018 1 12 0 &
 python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp         2018 2018 1 12 0 &
 
-# -year 2019, month 1 only
-python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp_2m    2019 2019 1  1 0 &
-python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_u 2019 2019 1  1 0 &
-python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_v 2019 2019 1  1 0 &
-python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp         2019 2019 1  1 0 &
+# -year 2019, month 1 to 12
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp_2m    2019 2019 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_u 2019 2019 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_v 2019 2019 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp         2019 2019 1 12 0 &
+
+# -year 2020, month 1 only
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp_2m    2020 2020 1  1 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_u 2020 2020 1  1 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_v 2020 2020 1  1 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp         2020 2020 1  1 0 &
 
 wait
 
