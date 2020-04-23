1f9b60c219aeeae438fdda095fe3b20f72a4da8f
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-04-23 10:53:55 +0200

    start downloading

--- process_era5-land/download_global_2016-2018.sh
+++ process_era5-land/download_global_2016-2018.sh
@@ -1,20 +1,23 @@
 
 set -x
 
-#~ # -year 2016, months 1 to 12
-#~ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp 2016 2016 1 12 0 &
-#~ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind 2016 2016 1 12 0 &
-#~ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp   2016 2016 1 12 0 &
+# -year 2016, months 1 to 12
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp_2m    2016 2016 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_u 2016 2016 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_v 2016 2016 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp         2016 2016 1 12 0 &
 
-#~ # -year 2017, months 1 to 12
-#~ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp 2017 2017 1 12 0 &
-#~ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind 2017 2017 1 12 0 &
-#~ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp   2017 2017 1 12 0 &
+# -year 2017, month 1 to 12
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp_2m    2017 2017 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_u 2017 2017 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_v 2017 2017 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp         2017 2017 1 12 0 &
 
-#~ # -year 2018, month 1 to 12
-#~ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp 2018 2018 1 12 0 &
-#~ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind 2018 2018 1 12 0 &
-#~ python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp   2018 2018 1 12 0 &
+# -year 2018, month 1 to 12
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp_2m    2018 2018 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_u 2018 2018 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ wind_10m_v 2018 2018 1 12 0 &
+python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ tp         2018 2018 1 12 0 &
 
 # -year 2019, month 1 only
 python download_era5-land_global.py /scratch/depfg/sutan101/era5-land_meteo/hourly/ temp_2m    2019 2019 1  1 0 &
