83ce4442bf2643ea968ff93471e05d81c2ccd50e
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-04-23 11:06:53 +0200

    start downloading - 2016-2020

--- process_era5-land/download_era5-land_global.py
+++ process_era5-land/download_era5-land_global.py
@@ -348,8 +348,8 @@ def main():
 
           params['day'] = ["{:02d}".format(i) for i in range(1,numberOfDays+1)]
           
-          # - for testing
-          params['day'] = ["{:02d}".format(i) for i in range(1,3+1)]
+          #~ # - for testing
+          #~ params['day'] = ["{:02d}".format(i) for i in range(1,3+1)]
 
           lastDate = '%04d%02d%02d' % (year, month, numberOfDays)
           if args.update == 0:
