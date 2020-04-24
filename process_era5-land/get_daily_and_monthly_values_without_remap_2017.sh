433d8f777b01aa8a976a9c1b85c47ff01deed0f7
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-04-24 17:34:14 +0200

    start calculating for Meng etc

--- process_era5-land/get_daily_and_monthly_values_without_remap_2017.sh
+++ process_era5-land/get_daily_and_monthly_values_without_remap_2017.sh
@@ -22,17 +22,17 @@ OUTPUT_FOLDER=${OUTPUT_FOLDER}/${YEAR}
 mkdir -p ${OUTPUT_FOLDER}
 
 
-# daily tp, total precipitation
-HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_tpre
-# - daily total precipitation, NOTE: using daymax (as hourly source data are accumulative on each day)
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/tanzania_era5-land_daily_total-preci_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,m.day-1 -daymax -selyear,${YEAR} -shifttime,-25min -selvar,tp -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} 
-
-# monthly tp, total precipitation
-MONTHLY_UNIT="m.month-1"
-CDOMON_OPERA="monsum"
-MONTH_OUTPUT_FILE=${OUTPUT_FOLDER}/tanzania_era5-land_monthly_total-preci_${YEAR}.nc
-CDO_TIMESTAT_DATE='last' cdo -L -settime,00:00:00 -setunit,${MONTHLY_UNIT} -${CDOMON_OPERA} ${DAILY_OUTPUT_FILE} ${MONTH_OUTPUT_FILE} 
+#~ # daily tp, total precipitation
+#~ HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_tpre
+#~ # - daily total precipitation, NOTE: using daymax (as hourly source data are accumulative on each day)
+#~ DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/tanzania_era5-land_daily_total-preci_${YEAR}.nc
+#~ cdo -L -b F64 -settime,00:00:00 -setunit,m.day-1 -daymax -selyear,${YEAR} -shifttime,-25min -selvar,tp -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} 
+#~ 
+#~ # monthly tp, total precipitation
+#~ MONTHLY_UNIT="m.month-1"
+#~ CDOMON_OPERA="monsum"
+#~ MONTH_OUTPUT_FILE=${OUTPUT_FOLDER}/tanzania_era5-land_monthly_total-preci_${YEAR}.nc
+#~ CDO_TIMESTAT_DATE='last' cdo -L -settime,00:00:00 -setunit,${MONTHLY_UNIT} -${CDOMON_OPERA} ${DAILY_OUTPUT_FILE} ${MONTH_OUTPUT_FILE} 
 
 
 # daily temp_2m
